40b73f62400b10dfe71be20cf8e7845d4150082f
Mark Allison <mark@stylingandroid.com>
2020-12-05 10:38:09 +0000

    Initial commit

---
+++ app/src/main/java/com/stylingandroid/material/timepicker/MaterialTimepickerApplication.kt
@@ -0,0 +1,7 @@
+package com.stylingandroid.material.timepicker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MaterialTimepickerApplication : Application()
